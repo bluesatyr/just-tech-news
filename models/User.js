4f80b8db70f1d5ff43a40ed85d635f44e7f841e8
bluesatyr <shawnevans.music@gmail.com>
2021-03-01 17:39:33 -0500

    create api routes

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Mosel, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create user model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        {
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [4] // password must be more than four characters long
+                }
+            }
+        }
+    },
+    {
+        //TABLE CONFIGURATION OPTIONS GO HERE
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscres instead of camel-casing
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
