5f3e0f1c0a8289b5a22478996614c94a1c91d260
bluesatyr <shawnevans.music@gmail.com>
2021-03-04 21:36:46 -0500

    create post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id' // points to user_id in the Post model
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id' // points to user_id in itself
+})
+
 module.exports ={ User, Post };
