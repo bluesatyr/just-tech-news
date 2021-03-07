f3ed7e2c74590aec6a4e714ff7b02eb0efe96762
bluesatyr <shawnevans.music@gmail.com>
2021-03-07 18:30:10 -0500

    create model and routes for comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
 const { route } = require('./user-routes');
 const sequelize = require('../../config/connection');
 
@@ -7,21 +7,30 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
     console.log('==========');
     Post.findAll({
-        attributes: [
-            'id', 
-            'post_url', 
-            'title', 
-            'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
         order: [['created_at', 'DESC']],
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
-            {
-                model: User,
-                attributes: ['username']
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
             }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
         ]
-    }).then(dbPostData => res.json(dbPostData))
+       }).then(dbPostData => res.json(dbPostData))
       .catch(err => {
           console.log(err);
           res.status(500).json(err);
