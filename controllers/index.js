9a961e1be443031cfc05164f1532e1dbd7dadda1
bluesatyr <shawnevans.music@gmail.com>
2021-03-08 19:02:41 -0500

    add render homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // a call to an endpoint that doesn't exist returns a 404 error
 router.use((req, res) => {
     res.status(404).end();
 });
+
 
 module.exports = router;
